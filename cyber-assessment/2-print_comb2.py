fbb887b2f68dcb3f25f56f030fd5bf6afe8ec06d
Massimo <massimo@Massimos-MacBook-Air.local>
2026-03-12 23:58:42 +0100

    v2.0

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print(f"{i:02d}", end=", ")
-    else:
-        print(f"{i:02d}")
+for i in range(99):
+	print(f"{i:02d}", end=", ")
+print("99")
