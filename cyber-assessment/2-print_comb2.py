f5f8e1a2dd14824542f9cd7f7e79a061bf5c803a
Massimo <massimo@Massimos-MacBook-Air.local>
2026-03-13 00:36:45 +0100

    rev-task3

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("%02d, " % i, end="")
-print("%02d" % 99)
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
